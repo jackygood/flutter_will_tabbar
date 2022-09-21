b89cfb6d40f6167cd478a8ec67b7b6ca84d6e238
Antonio <adepalma07@gmail.com>
2022-09-21 09:34:07 +0200

    migration to null safety

--- lib/will_tabbar.dart
+++ lib/will_tabbar.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 class WillTabBar extends StatefulWidget {
-
   ///The function that is to be called when the current index/value of this [WillTabBar] changes.
-  final Function(int) onTap;
+  final Function(int)? onTap;
 
   ///The function that is to be used to get the current index/value of this [WillTabBar].
-  final Function currentValue;
+  final Function? currentValue;
 
   ///
   final List<String> tabBarItemTitles;
@@ -33,29 +32,30 @@ class WillTabBar extends StatefulWidget {
   ///
   final double horizontalMargin;
 
-  const WillTabBar({
-    Key key,
-    this.tabBarItemTitles,
-    this.onTap,
-    this.currentValue,
-    this.customBarWidth = 0.5,
-    this.normalTextColor = const Color(0xffA1A8BD),
-    this.normalTextFontSize = 16,
-    this.selectTextColor = const Color(0xff0062E0),
-    this.selectTextFontSize = 16,
-    this.horizontalMargin = 10,
-    this.customBarColor = const Color(0xff0062E0)
-  }) : assert(tabBarItemTitles != null,'tabBarItemTitles must not be null'),assert(tabBarItemTitles.length < 6,'tabBarItemTitles must be less than 5');
+  const WillTabBar(
+      {Key? key,
+      required this.tabBarItemTitles,
+      this.onTap,
+      this.currentValue,
+      this.customBarWidth = 0.5,
+      this.normalTextColor = const Color(0xffA1A8BD),
+      this.normalTextFontSize = 16,
+      this.selectTextColor = const Color(0xff0062E0),
+      this.selectTextFontSize = 16,
+      this.horizontalMargin = 10,
+      this.customBarColor = const Color(0xff0062E0)})
+      : assert(tabBarItemTitles.length < 6,
+            'tabBarItemTitles must be less than 5');
 
   @override
   _WillTabBarState createState() => _WillTabBarState();
 }
 
 class _WillTabBarState extends State<WillTabBar> {
-  List<GlobalKey> _globalKeys;
-  bool _showSelf;
-  double _maxWidth;
-  double _maxHeight;
+  late List<GlobalKey> _globalKeys;
+  late bool _showSelf;
+  double? _maxWidth;
+  late double _maxHeight;
   double _barH = 5;
 
   int selectIndex = 0;
@@ -72,15 +72,16 @@ class _WillTabBarState extends State<WillTabBar> {
         _maxHeight = 0;
         for (int i = 0; i < _globalKeys.length; i++) {
           RenderBox _renderBox =
-          _globalKeys[i].currentContext.findRenderObject();
-          if (_renderBox.size.width > _maxWidth) {
+              _globalKeys[i].currentContext!.findRenderObject() as RenderBox;
+          if (_renderBox.size.width > _maxWidth!) {
             _maxWidth = _renderBox.size.width;
           }
           if (_renderBox.size.height > _maxHeight) {
             _maxHeight = _renderBox.size.height;
           }
         }
-        _maxWidth = _maxWidth - (widget.horizontalMargin*2/widget.tabBarItemTitles.length);
+        _maxWidth = _maxWidth! -
+            (widget.horizontalMargin * 2 / widget.tabBarItemTitles.length);
         _showSelf = true;
       });
     }
@@ -88,7 +89,7 @@ class _WillTabBarState extends State<WillTabBar> {
 
   void initState() {
     super.initState();
-    _globalKeys = List<GlobalKey>();
+    _globalKeys = <GlobalKey>[];
     for (int i = 0; i < widget.tabBarItemTitles.length; i++) {
       _globalKeys.add(GlobalKey());
     }
@@ -96,35 +97,42 @@ class _WillTabBarState extends State<WillTabBar> {
     WidgetsBinding.instance.addPostFrameCallback(onPostFrameCallback);
   }
 
-  List<Widget> tabBars() => List<Widget>.generate(widget.tabBarItemTitles.length, (int index) {
-    return Expanded(
-        flex: 1,
-        child: GestureDetector(
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  widget.tabBarItemTitles[index],
-                  style: index == widget.currentValue()
-                      ? TextStyle(fontSize: widget.selectTextFontSize, color: widget.selectTextColor)
-                      : TextStyle(fontSize: widget.normalTextFontSize, color: widget.normalTextColor),
-                )
-              ],
-            ),
-          ),
-          onTap: (){
-            widget.onTap(index);
-          },
-        )
-    );
-  });
+  List<Widget> tabBars() =>
+      List<Widget>.generate(widget.tabBarItemTitles.length, (int index) {
+        return Expanded(
+            flex: 1,
+            child: GestureDetector(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      widget.tabBarItemTitles[index],
+                      style: index == widget.currentValue!()
+                          ? TextStyle(
+                              fontSize: widget.selectTextFontSize,
+                              color: widget.selectTextColor)
+                          : TextStyle(
+                              fontSize: widget.normalTextFontSize,
+                              color: widget.normalTextColor),
+                    )
+                  ],
+                ),
+              ),
+              onTap: () {
+                widget.onTap!(index);
+              },
+            ));
+      });
 
   Alignment _getAlignment() {
-    return Alignment(-1.0 + widget.currentValue() / (widget.tabBarItemTitles.length - 1) * 2, 1.0);
+    return Alignment(
+        -1.0 +
+            widget.currentValue!() / (widget.tabBarItemTitles.length - 1) * 2,
+        1.0);
   }
 
   @override
@@ -152,22 +160,25 @@ class _WillTabBarState extends State<WillTabBar> {
       );
     } else {
       return Container(
-        margin: EdgeInsets.fromLTRB(widget.horizontalMargin,widget.horizontalMargin,widget.horizontalMargin,0),
+        margin: EdgeInsets.fromLTRB(widget.horizontalMargin,
+            widget.horizontalMargin, widget.horizontalMargin, 0),
         height: 50,
         decoration: BoxDecoration(
             color: Colors.white,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(14),topRight: Radius.circular(14))
-        ),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(14), topRight: Radius.circular(14))),
         child: Stack(
           alignment: AlignmentDirectional.centerStart,
           children: <Widget>[
             Container(
               decoration: BoxDecoration(
-                  border: Border(bottom: BorderSide(color: Color(0xffeeeeee),width: 1,style: BorderStyle.solid),)
-              ),
-              child: Row(
-                  children: tabBars()
-              ),
+                  border: Border(
+                bottom: BorderSide(
+                    color: Color(0xffeeeeee),
+                    width: 1,
+                    style: BorderStyle.solid),
+              )),
+              child: Row(children: tabBars()),
             ),
             AnimatedAlign(
               alignment: _getAlignment(),
@@ -178,21 +189,21 @@ class _WillTabBarState extends State<WillTabBar> {
                 child: Row(
                   children: <Widget>[
                     Container(
-                      width: _maxWidth*(1-widget.customBarWidth)/2,
+                      width: _maxWidth! * (1 - widget.customBarWidth) / 2,
                       height: _barH,
                       decoration: BoxDecoration(
                         color: Colors.transparent,
                       ),
                     ),
                     Container(
-                      width: _maxWidth*widget.customBarWidth,
+                      width: _maxWidth! * widget.customBarWidth,
                       height: _barH,
                       decoration: BoxDecoration(
                         color: widget.customBarColor,
                       ),
                     ),
                     Container(
-                      width: _maxWidth*(1-widget.customBarWidth)/2,
+                      width: _maxWidth! * (1 - widget.customBarWidth) / 2,
                       height: _barH,
                       decoration: BoxDecoration(
                         color: Colors.transparent,
